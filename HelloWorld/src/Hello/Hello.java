d620581aedbd4d6727b8907365349074942377c3
Ruslan <90256075+Ruslan112mk@users.noreply.github.com>
2021-09-16 18:31:38 +0300

    t
    t

---
+++ HelloWorld/src/Hello/Hello.java
@@ -0,0 +1,17 @@
+package Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+			
+		System.out.println("h  h eeee l     l     oooo");
+		System.out.println("h  h e    l     l     o  o");
+		System.out.println("hhhh eeee l     l     o  o");
+		System.out.println("h  h e    l     l     o  o");
+		System.out.println("h  h eeee lllll lllll oooo");
+			
+			}
+			
+}
+	
